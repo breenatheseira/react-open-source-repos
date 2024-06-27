bb7aabf83cd348642b7f6cfca22cc048bbc36e74
Breena <breenatheseira@gmail.com>
2024-06-28 00:42:51 +0800

    Configuring deployment through Github Actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_REPO_NAME,
 })

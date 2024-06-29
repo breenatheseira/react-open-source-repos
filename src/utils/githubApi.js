9c0c1a7351ff6c15d217bae52af966459d83da11
Breena <breenatheseira@gmail.com>
2024-06-29 11:12:22 +0800

    Call GithubAPI for live repos

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
 
-function GithubApi() {
-  return '';
+// https://api.github.com/orgs/reactjs/repos?sort=full_name&page=1
+const allReactReposPath = '/orgs/reactjs/repos';
+
+// https://api.github.com/search/repositories?page=1&q=t+in:name+org:reactjs
+const searchRepo = '/search/repositories';
+
+const options = {
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Accept': 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28',
+  }
 }
 
-export default GithubApi;
+async function getAllRepos(pageNumber = 1){
+  options.params = {
+    sort: 'full_name',
+    page: pageNumber
+  };
+
+  return axios.get(allReactReposPath, options);
+}
+
+export default getAllRepos;

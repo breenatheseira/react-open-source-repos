55781e26e33fea380f21f3f6dc1274b842a8ee39
Breena <breenatheseira@gmail.com>
2024-07-03 11:58:46 +0800

    Fixing badges alignment

--- src/features/repositories/RepositoryItem.jsx
+++ src/features/repositories/RepositoryItem.jsx
@@ -4,33 +4,44 @@ import eyeIcon from '../../assets/eye.svg'
 import starIcon from '../../assets/star.svg'
 import forkIcon from '../../assets/fork.svg'
 
+const BadgeStat = ({ stat, icon, text }) => {
+  return (
+    // <div className="badge bg-light text-secondary text-wrap align-self-center fs-7 me-2">
+    <div className="badge bg-dark text-secondary text-wrap align-self-center fs-7 me-2">
+      <span>{stat}</span>
+      <img src={icon} alt='eye icon' className='pb-1 ps-2 pe-1'/>
+      <span className='d-none d-md-inline-block'>{text}</span>
+    </div>
+  )
+}
+
 const RepositoryItem = ({ repo }) => {
   return (
     <Accordion.Item eventKey={repo.id}>
       <Accordion.Header>{repo.name}</Accordion.Header>
       <Accordion.Body>
         <div className='d-flex pb-3'>
-          <div className="flex-grow-1 bd-highlight" style={{width: '60px'}}>
+          <div className="flex-grow-1 bd-highlight" style={{width: '50px'}}>
             <span>
               <a href={repo.html_url} target='_blank' className='mb-2'>Check it out</a>
             </span>
           </div>
-          <div>
-            <div className="badge bg-light text-secondary text-wrap align-self-center fs-7 w-80 me-2">
-              {repo.watchers_count}
-              <img src={eyeIcon} alt='eye icon' className='pb-1 ps-2 pe-1'/>
-              Watchers
-            </div>
-            <div className="badge bg-light text-secondary text-wrap align-self-center fs-7 w-80 me-2">
-              {repo.stargazers_count}
-              <img src={starIcon} alt='star icon' className='pb-1 ps-2 pe-1'/>
-              Starred
-            </div>
-            <div className="badge bg-light text-secondary text-wrap align-self-center fs-7 w-80">
-              {repo.forks_count}
-              <img src={forkIcon} alt='fork icon' className='pb-1 ps-2 pe-1'/>
-              Forked
-            </div>
+          <div className=''>
+            <BadgeStat
+              stat={repo.watchers_count}
+              icon={eyeIcon}
+              text={'Watchers'}
+            />
+            <BadgeStat
+              stat={repo.stargazers_count}
+              icon={starIcon}
+              text={'Starred'}
+            />
+            <BadgeStat
+              stat={repo.forks_count}
+              icon={forkIcon}
+              text={'Forked'}
+            />
           </div>
         </div>
         <div>

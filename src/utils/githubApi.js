6aae6ad010c116eeed6ef8c97d5e23dd17ecfeca
Breena <breenatheseira@gmail.com>
2024-07-05 02:57:20 +0800

    Refactored githubApi to be cleaner

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,14 +1,5 @@
 import axios from 'axios'
 
-// https://api.github.com/orgs/reactjs/repos?sort=full_name&page=1
-const allReactReposPath = '/orgs/reactjs/repos';
-
-// https://api.github.com/repos/reactjs/ar.react.dev
-const getRepoByIdPath = '/repos/'
-
-// https://api.github.com/search/repositories?page=1&q=in:name+org:reactjs+t
-const searchRepoPath = '/search/repositories';
-
 const options = {
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
@@ -21,29 +12,31 @@ if(import.meta.env.VITE_GITHUB_BEARER_TOKEN){
   options.headers['Authorization'] = 'Bearer ' + import.meta.env.VITE_GITHUB_BEARER_TOKEN
 }
 
-export async function fetchInitialRepos(page = 1){
-  const path = allReactReposPath
-  const requestOptions = { ...options }
-
-  requestOptions.params = {
-    sort: 'full_name',
-    page
-  };
-
-  return axios.get(path, requestOptions);
+const paths = {
+  // https://api.github.com/orgs/reactjs/repos?sort=full_name&page=1
+  fetchRepositories: (page) => `/orgs/reactjs/repos?sort=full_name&page=${page}`,
+  // https://api.github.com/repos/reactjs/ar.react.dev (full_name: reactjs/ar.react.dev)
+  fetchOneRepository: (fullName) => `/repos/${fullName}`,
+  // https://api.github.com/search/repositories?page=1&q=in:name+org:reactjs+t (query: t)
+  searchRepositories: (query, page) => `/search/repositories?page=${page}&q=${query} in:name org:reactjs`
 }
 
-export async function fetchOneRepo(fullName){
-  const path = getRepoByIdPath + fullName
-  const requestOptions = { ...options }
-
-  return axios.get(path, requestOptions)
+const githubApi = {
+  async fetchRepos(page = 1){
+    return await axios.get(paths.fetchRepositories(page), options)
+      .then(response => response)
+      .catch(error => { throw error })
+  },
+  async fetchOneRepo(name){
+    return await axios.get(paths.fetchOneRepository(name), options)
+      .then(response => response)
+      .catch(error => { throw error })
+  },
+  async searchForRepos(query, page = 1){
+    return await axios.get(paths.searchRepositories(query, page), options)
+      .then(response => response)
+      .catch(error => { throw error })
+  }
 }
 
-export async function searchForRepository(query, page = 1){
-  const searchQuery = 'in:name org:reactjs ' + query
-  const searchOptions = { ...options }
-  searchOptions.params = { q: searchQuery, page }
-  
-  return axios.get(searchRepoPath, searchOptions)
-}
+export default githubApi

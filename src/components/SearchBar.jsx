14b35bc0cc5bc08e2c5449ea0f694996f06aaed9
Breena <breenatheseira@gmail.com>
2024-07-04 12:57:03 +0800

    Can search without pressing Enter key

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,25 @@
+import useDebounce from "../hooks/useDebounce";
+
 const SearchBar = ({ searchText, onSearchTextChange, onEnterDown }) => {
+
+  const debouncedRequest = useDebounce(() => {
+    // send request to the backend
+    // access to latest state here
+    const trimmedText = searchText.trim()
+    console.log(trimmedText);
+    onEnterDown(trimmedText)
+  });
 
   function handleOnChange(e){ 
     onSearchTextChange(e.target.value) 
-  }
-  function handleOnKeyDown(e){
-    const trimmedText = e.target.value.trim()
-
-    // If user pressed Enter key
-    if (e.which === 13 && trimmedText) {
-      onEnterDown(trimmedText)
-    }
-  }
+    debouncedRequest()
+  }  
 
   return (
     <input 
       className="form-control form-control-lg"
       type="text" 
       value={searchText}
-      onKeyDown={handleOnKeyDown}
       onChange={handleOnChange}
       placeholder="Search..."
     />
